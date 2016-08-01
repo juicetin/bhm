fixed use of symbols - hard coded close form solutions for derivatives now

ML/gp.py
@@ -1,16 +1,21 @@
-import numpy as np
 import sys
+import math
+from datetime import datetime
+
+# Numpy
+import numpy as np
 from numpy import linalg
+
+# Numpy
+import sympy as sp
 from sympy import KroneckerDelta
+from sympy.utilities.autowrap import autowrap
+from sympy.utilities.lambdify import lambdify, implemented_function
+
+# Scipy
 from scipy.optimize import minimize
 from scipy.spatial.distance import cdist
-from sympy.utilities.lambdify import lambdify, implemented_function
 from scipy.stats import norm
-import math
-import sympy
-from sympy.utilities.autowrap import autowrap
-
-from datetime import datetime
 
 # TODO when doing K(X, X), simply use (n_err^2)I instead of KroneckerDelta
 
@@ -21,6 +26,13 @@ from datetime import datetime
 # a = a.reshape(5,1) # 5 rows, 1 column
 # b = np.array([6,7,8])
 # b = np.reshape(1,3) # 1 row, 3 columns
+
+def timing(f):
+    def wrap(*args):
+        t1 = datetime.now()
+        ret = f(*args)
+        t2 = datetime.now()
+        print("{} function took {}".format(f.func_name, t2-t1))
 
 class GaussianProcess:
     def __init__(self):
@@ -37,10 +49,10 @@ class GaussianProcess:
     #     self.size = len(X)
 
     #     # Pre-calculate derivatives of inverted matrix to substitute values in the Squared Exponential NLL gradient
-    #     # self.f_err_sym, self.l_scale_sym, self.n_err_sym = sympy.symbols("f_err, l_scale, n_err")
-    #     self.f_err_sym, self.n_err_sym = sympy.symbols("f_err, n_err")
-    #     self.l_scale_sym = sympy.MatrixSymbol('l', 1, self.size)
-    #     m = sympy.Matrix(self.f_err_sym**2 * math.e**(-0.5 * self.dist(self.X/self.l_scale_sym, self.X/self.l_scale_sym)) 
+    #     # self.f_err_sym, self.l_scale_sym, self.n_err_sym = sp.symbols("f_err, l_scale, n_err")
+    #     self.f_err_sym, self.n_err_sym = sp.symbols("f_err, n_err")
+    #     self.l_scale_sym = sp.MatrixSymbol('l', 1, self.size)
+    #     m = sp.Matrix(self.f_err_sym**2 * math.e**(-0.5 * self.dist(self.X/self.l_scale_sym, self.X/self.l_scale_sym)) 
     #                      + self.n_err_sym**2 * np.identity(self.size))
     #     self.dK_dthetas = [
     #                  m.diff(self.f_err_sym),
@@ -79,48 +91,61 @@ class GaussianProcess:
         var = np.diag(self.K_se(x, x, self.f_err, self.l_scales) - v.T.dot(v))
         return fs_mean, var
 
-    def se_term(self, x1, x2, l_scales):
+    def dist(self, x1, x2, l_scales):
         # Dividing by length scale first before passing into cdist to
         #   accounts for different length scale for each dimension
-        # return self.sqeucl_dist(x1/l_scales, x2/l_scales)
-        return cdist(x1/l_scales, x2/l_scales)
+        return cdist(x1/l_scales, x2/l_scales, 'sqeuclidean')
 
     # NOTE cdist can't deal with sympy symbols :(
     def sqeucl_dist(self, x, xs, x_len=0, xs_len=0):
-        # BAD. Need to get cdist to work with symbols, or make this way more efficient
-        # m = np.sum( np.power([i-j for i in x for j in xs], 2), axis=1).reshape(x.shape[0], xs.shape[0])
-
-        # if self.lib == 'sympy':
         m = np.sum(np.power(
-            np.repeat(x[:,None,:], len(self.X), axis=1) - 
-            np.resize(xs, (len(self.X), xs.shape[0], xs.shape[1])), 
+            np.repeat(x[:,None,:], len(x), axis=1) - 
+            np.resize(xs, (len(x), xs.shape[0], xs.shape[1])), 
             2), axis=2)
 
         return m
 
-        # return cdist(x, xs, 'sqeuclidean')
-        # return (x - xs.T)**2
-
     def K_se(self, x1, x2, f_err, l_scales):
-
-        # m = np.array(self.se_term(x1, x2, l_scales), dtype=np.float32)
-        m = self.se_term(x1, x2, l_scales).astype(float)
-        # m = cdist(x1/l_scales, x2/l_scales)
-        # return np.array((f_err**2) * np.exp(-0.5 * m), dtype=np.float64)
+        m = self.dist(x1, x2, l_scales)
         return f_err**2 * np.exp(-0.5 * m)
+
+    def dK_df_eval(self, m, f_err, l_scales):
+        return 2*f_err * m
+
+    def dK_dls_eval(self, k, f_err, l_scales):
+
+        k_ = np.copy(k)
+        k_ = f_err**2 * k_
+
+        # Repeats each row along axis=1
+        M = np.repeat(self.X[:,None,:], len(self.X), axis=1) 
+
+        # Separates Ms into every dimension of original dataset
+        M_ds = np.array([[M[:,:,i], M[:,:,i].T] for i in range(self.X.shape[1])]) 
+
+        # Derivative over length scale for each dimension
+        dK_dls = [l_scale**(-3) * (m-mt)**2 * k_ for l_scale, (m, mt) in zip(l_scales, M_ds)]
+
+        return dK_dls
+
+    def dK_dn_eval(self, n_err):
+        dK_dn = np.diag(np.array([2*n_err]*self.X.shape[0]))
+        return dK_dn
 
     # Evaluates each dK_dtheta pre-calculated symbolic lambda with current iteration's hyperparameters
     def eval_dK_dthetas(self, f_err, l_scales, n_err):
-        # NOTE won't need to branch here in future
-        if self.eval == 'autowrap':
-            l_scales = l_scales.reshape(1, len(l_scales))
-            dK_df_eval = self.dK_df(f_err, l_scales)
-            dK_dl_evals = [dK_dl(f_err, l_scales) for dK_dl in self.dK_dls]
-            dK_dn_eval = self.dK_dn(n_err)
-            return np.array([dK_df_eval] + dK_dl_evals + [dK_dn_eval], dtype=np.float64)
-        else:
-            l_scales = sympy.Matrix(l_scales.reshape(1, len(l_scales)))
-            return np.array(self.dK_dthetas(f_err, l_scales, n_err), dtype=np.float64)
+        # Reshape length scales into a 1x matrix
+        l_scales = np.array(l_scales)
+
+        # exp(...) block of squared exponential function
+        m = np.exp(-0.5 * self.dist(self.X, self.X, l_scales))
+
+        # Evaluate all the partial derivatives
+        dK_df = self.dK_df_eval(m, f_err, l_scales)
+        dK_dls = self.dK_dls_eval(m, f_err, l_scales)
+        dK_dn = self.dK_dn_eval(n_err)
+
+        return np.array([dK_df] + dK_dls + [dK_dn], dtype=np.float64)
 
     # def SE_der(self, args):
     #     # TODO fix - get around apparent bug
@@ -193,6 +218,9 @@ class GaussianProcess:
             np.sqrt(1 + a**2 * sigma_sq)
         ))
 
+        # grads = self.LLOO_der(args)
+        # print(grads)
+
         return LLOO
 
     #################### Derivative ####################
@@ -203,7 +231,7 @@ class GaussianProcess:
         
         self.count += 1
         # print("Iterated [{}] times for current fitting...".format(self.count))
-        print(self.count, end=" ", flush=True)
+        # print(self.count, end=" ", flush=True)
 
         f_err, l_scales, n_err, a, b = self.unpack_LLOO_args(args)
 
@@ -260,74 +288,21 @@ class GaussianProcess:
 
     #################### Prediction ####################
 
-    def build_symbolic_derivatives(self):
-        # Pre-calculate derivatives of inverted matrix to substitute values in the Squared Exponential NLL gradient
-        self.f_err_sym, self.n_err_sym = sympy.symbols("f_err, n_err")
-
-        # (1,n) shape 'matrix' (vector) of length scales for each dimension
-        self.l_scale_sym= sympy.MatrixSymbol('l', 1, self.X.shape[1])
-
-        # (n, n) shape matrix of data
-        self.data_sym = sympy.MatrixSymbol('m', self.X.shape[0], self.X.shape[1])
-
-        # K matrix
-        print("Building sympy matrix...")
-        m_t1 = datetime.now()
-        sq_eucl_dists = self.sqeucl_dist(self.X/self.l_scale_sym, self.X/self.l_scale_sym)
-        m = sympy.Matrix(self.f_err_sym**2 * math.e**(-0.5 * sq_eucl_dists) + self.n_err_sym**2 * np.identity(self.size))
-        m_t2 = datetime.now()
-        print("Took: {}".format(m_t2 - m_t1))
-
-        # Element-wise derivative of K matrix over each of the hyperparameters
-        print("Getting partial derivatives over all hyperparameters...")
-        pd_t1 = datetime.now()
-        print("Getting dK_df...")
-        dK_df   = m.diff(self.f_err_sym)
-        print("Getting dK_dls...")
-        dK_dls  = [m.diff(l_scale_sym) for l_scale_sym in self.l_scale_sym]
-        print("Getting dK_dn...")
-        dK_dn   = m.diff(self.n_err_sym)
-        print("Took: {}".format(datetime.now() - pd_t1))
-
-        # Lambdify each of the dK/dts to speed up substitutions per optimization iteration
-        print("Lambdifying/Autowrapping...")
-        l_t1 = datetime.now()
-        self.dK_dthetas = [dK_df] + dK_dls + [dK_dn]
-
-        # NOTE Won't need to branch here in future, always autowrap
-        if self.eval == 'autowrap':
-            # self.dK_dthetas = [autowrap(dk_dt, backend='cython') for dK_dt in self.dK_dthetas]
-            print("Autowrapping dK_df...")
-            self.dK_df = autowrap(dK_df)
-            print("Autowrapping dK_dls...")
-            self.dK_dls = [autowrap(dK_dl) for dK_dl in dK_dls]
-            print("Autowrapping dK_dn...")
-            self.dK_dn = autowrap(dK_dn)
-        else:
-            self.dK_dthetas = sympy.lambdify((self.f_err_sym, self.l_scale_sym, self.n_err_sym), self.dK_dthetas, 'numpy')
-        print("Took: {}".format(datetime.now() - l_t1))
-        # self.dK_dthetas = sympy.lambdify((self.f_err_sym, self.l_scale_sym, self.n_err_sym), self.dK_dthetas, [{'ImmutableMatrix':np.array, "numpy"])
-
     # Classification
     def fit_class(self, X, y):
-        self.lib = 'sympy'
-        # self.eval = 'sympy'
-        self.eval = 'autowrap'
-
-        self.count = 0
 
         self.size = len(y)
         self.X = X
         self.classifier_params = {}
         params = ['f_err', 'l_scales', 'n_err', 'a', 'b']
 
-        # Build lambdified partial derivatives
-        self.build_symbolic_derivatives()
-
         # Build OvA classifier for each unique class in y
         print("Starting to build OvA classifier per class...")
-        print("Current iterations... ", end=" ", flush=True)
+        # print("Current iterations... ", end=" ", flush=True)
         for c in set(y):
+            # Count iterations needed per optimize.minimize
+            self.count = 0
+
             self.classifier_params[c] = {}
 
             # f_err, l_scales (for each dimension), n_err, alpha, beta
@@ -340,16 +315,20 @@ class GaussianProcess:
             # res = minimize(self.LLOO, x0, method='bfgs')
             res = minimize(self.LLOO, x0, method='bfgs', jac=self.LLOO_der)
 
+            print ("Iterations: {}".format(self.count))
+
             # Set params for current binary regressor (classifier)
             for param, val in zip(params, res['x']):
                 self.classifier_params[c][param] = val
+
+            print("Current params: {}".format(self.classifier_params[c]))
 
             # Reset ys
             self.y = y
         print()
 
     def predict_class(self, x):
-        print("Original: {}".format(self.y))
+        # print("Original classes: {}".format(self.y))
 
         # Copy y for modification and resetting/restoring
         y = np.copy(self.y)
@@ -360,9 +339,11 @@ class GaussianProcess:
             for label, params in self.classifier_params.items()
         ]
 
-        print("Regression values for binary cases:")
-        for y_pred in y_preds:
-            print(y_pred)
+        # Printed in loop to format on each row nicely
+        # print("-----Regression values for binary cases-----")
+        # for y_pred in y_preds:
+        #     print(y_pred)
+        # print("--------------------------------------------")
 
         # Return max squashed value for each data point representing class prediction
         return np.argmax(y_preds, axis=0)
@@ -383,6 +364,7 @@ class GaussianProcess:
         # Get predictions of resulting mean and variances
         y_pred, var = self.predict(x)
         # sigma = np.sqrt(var)
+        # print(y_pred)
         y_squashed = self.sigmoid(y_pred)
 
         # Restore y

loaddata.py
@@ -9,6 +9,11 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import scale
 from sklearn.svm import SVC
 from sklearn.cross_validation import train_test_split
+from sklearn.cross_validation import StratifiedShuffleSplit
+from sklearn.cross_validation import LeaveOneOut
+from sklearn.cross_validation import StratifiedKFold
+from sklearn import datasets
+from datetime import datetime
 
 from matplotlib import pyplot as plt
 
@@ -17,6 +22,7 @@ from ML.validation import cross_validate_algo
 from ML.knn import kNN
 from ML.random_forests import rf
 from ML.logistic_regression import lr
+from ML.gp import GaussianProcess
 
 import visualisation as vis
 
@@ -43,16 +49,127 @@ def load_test_data():
 
     return (qp_locations, validQueryID, x_bins, query, y_bins)
 
+def stratified_micro_batch(features, labels, point_count):
+    sss = StratifiedShuffleSplit(labels, 1, test_size=point_count/len(features))
+    for train_index, test_index in sss:
+        pass
+    return features[test_index], labels[test_index]
+
+def summarised_labels(labels):
+    label_map={1:0,2:0,3:1,4:3,5:1,6:3,7:3,8:3,9:3,10:1,11:3,12:3,13:2,14:2,15:2,16:1,17:1,18:0,19:1,20:0,21:0,22:1,23:0,24:0}
+    new_labels = np.copy(labels)
+    for k, v in label_map.items(): 
+        new_labels[labels==k] = v
+    return new_labels
+
+def classification_bathy_testing(features, labels):
+    ####################################################################################
+    # GP Classification on Bathymetry Data
+    ####################################################################################
+
+    gp = GaussianProcess()
+
+    # cv = LeaveOneOut(len(labels))
+    cv = StratifiedKFold(labels, n_folds=10)
+    # cv = StratifiedShuffleSplit(labels, 1, test_size=4/len(features))
+    accuracies = []
+    for train_index, test_index in cv:
+        X_train, X_test = features[train_index], features[test_index]
+        y_train, y_test = labels[train_index], labels[test_index]
+
+        gp.fit_class(X_train, y_train)
+        y_pred = gp.predict_class(X_test)
+
+        accuracy = gp.score(y_pred, y_test)
+        accuracies.append(accuracy)
+        print("Accuracy is: {}".format(accuracy))
+
+        print("P: {}\n, A: {}".format(y_pred, y_test))
+
+    print("Average accuracy: {}".format(np.average(accuracies)))
+
+def regression_dummy_testing():
+    ####################################################################################
+    # Dummy testing
+    ####################################################################################
+
+    a = np.array([i for i in range(1,10)]).reshape(3,3)
+    b = np.array([2,2,2])
+
+    from ML.gp import GaussianProcess
+    # from sklearn.gaussian_process import GaussianProcess
+    gp = GaussianProcess()
+    X = np.array([-1.50,-1.00,-0.75,-0.40,-0.25,0.00])
+    X = X.reshape(len(X), 1)
+    y = np.array([-1.70,-1.20,-0.25,0.30,0.5,0.7])
+    # y = y.reshape(len(y), 1)
+
+    # x = np.array([-1.45, -1.32, -0.5, -0.32, -0.10, 0.2, 0.3, 0.4])
+    x = np.array([0.2])
+    x = x.reshape(len(x), 1)
+    gp.fit(X, y)
+
+    y_pred, MSE = gp.predict(x)
+    sigma = np.sqrt(MSE)
+
+    # Plot function, prediction, and 95% confidence interval based on MSE
+    from visualisation import plot
+    # plot(X, y, x, y_pred, sigma)
+
+def classification_dummy_testing():
+    ####################################################################################
+    # Dummy testing - classification
+    ####################################################################################
+
+    gp = GaussianProcess()
+
+    #X, y = datasets.make_circles(n_samples=12)
+    # n_informative
+    # n_redundant
+    # n_repeated
+    num = 3
+    X, y = datasets.make_classification(n_samples=1000,
+            n_features=num, 
+            n_clusters_per_class=2,
+            n_redundant=0, 
+            n_repeated=0,
+            n_informative=num,
+            n_classes=num)
+
+    from sklearn.cross_validation import StratifiedKFold
+    cv = StratifiedKFold(y, n_folds = 20)
+    # cv = StratifiedShuffleSplit(y, 1, test_size=33/len(X))
+    accuracies = []
+    for train_index, test_index in cv:
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+
+        gp.fit_class(X_train, y_train)
+        y_pred = gp.predict_class(X_test)
+
+        accuracy = gp.score(y_pred, y_test)
+        accuracies.append(accuracy)
+        print("Accuracy is: {}".format(accuracy))
+
+    print("Average accuracy: {}".format(np.average(accuracies)))
 
 # Main function
 if __name__ == "__main__":
     # Load and assign all data
     # labels, labelcounts, bath_locations, features, querypoints_lowres, qp_locations, validQueryID, x_bins, query, y_bins = load_data()
+
+    print("Loading data from npzs...")
     labels, labelcounts, bath_locations, features = load_training_data()
     qp_locations, validQueryID, x_bins, query, y_bins = load_test_data()
 
+    print("Extracting features/etc...")
     features = np.array(features)
+    features = scale(normalize(features)) # Whiten
     labels = np.array(labels)
+    # labels = summarised_labels(labels)
+
+    mini_features, mini_labels = stratified_micro_batch(features, labels, 300)
+
     # x_bins_training, y_bins_training = np.meshgrid(list(set(bath_locations[:,0])), list(set(bath_locations[:,1])))
     # x_bins_training, y_bins_training = list(set(bath_locations[:,0])), list(set(bath_locations[:,1]))
 
@@ -63,7 +180,6 @@ if __name__ == "__main__":
     #         RandomForestClassifier(),                                       
     #         # SVC()
     #         ]
-
 
     # X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size = 0.2)
     # rf = RandomForestClassifier()
@@ -78,82 +194,9 @@ if __name__ == "__main__":
     # for classifier in classifiers:
     #     results.append(cross_validate_algo(features, labels, 10, classifier))
 
-    # Whiten
-    # features = scale(normalize(features))
-
     # # 10-fold cross-validation for all
     # for classifier in classifiers:
     #     cross_validate_algo(features, labels, 10, classifier)
 
-    ####################################################################################
-    # Dummy testing
-    ####################################################################################
-
-    # a = np.array([i for i in range(1,10)]).reshape(3,3)
-    # b = np.array([2,2,2])
-
-    # from ML.gp import GaussianProcess
-    # # from sklearn.gaussian_process import GaussianProcess
-    # gp = GaussianProcess()
-    # X = np.array([-1.50,-1.00,-0.75,-0.40,-0.25,0.00])
-    # X = X.reshape(len(X), 1)
-    # y = np.array([-1.70,-1.20,-0.25,0.30,0.5,0.7])
-    # # y = y.reshape(len(y), 1)
-
-    # # x = np.array([-1.45, -1.32, -0.5, -0.32, -0.10, 0.2, 0.3, 0.4])
-    # x = np.array([0.2])
-    # x = x.reshape(len(x), 1)
-    # gp.fit(X, y)
-
-    # y_pred, MSE = gp.predict(x)
-    # sigma = np.sqrt(MSE)
-
-    # # Plot function, prediction, and 95% confidence interval based on MSE
-    # from visualisation import plot
-    # # plot(X, y, x, y_pred, sigma)
-
-    ####################################################################################
-    # Dummy testing - classification
-    ####################################################################################
-
-    from ML.gp import GaussianProcess
-    gp = GaussianProcess()
-
-    from sklearn import datasets
-    from datetime import datetime
-
-    #X, y = datasets.make_circles(n_samples=12)
-    # n_informative
-    # n_redundant
-    # n_repeated
-    num = 4
-    X, y = datasets.make_classification(n_samples=150,
-            n_features=num, 
-            n_clusters_per_class=num,
-            n_redundant=0, 
-            n_repeated=0,
-            n_informative=num,
-            n_classes=num)
-
-    from sklearn.cross_validation import StratifiedKFold
-    kf = StratifiedKFold(y, n_folds = 10)
-    accuracies = []
-    for train_index, test_index in kf:
-        X_train, X_test = X[train_index], X[test_index]
-        y_train, y_test = y[train_index], y[test_index]
-
-        gp.fit_class(X_train, y_train)
-        y_pred = gp.predict_class(X_test)
-
-        accuracy = gp.score(y_pred, y_test)
-        accuracies.append(accuracy)
-        print("Accuracy is: {}".format(accuracy))
-
-        print(y_test)
-        print(y_pred)
-
-    print("Average accuracy: {}".format(np.average(accuracies)))
-
-    # d2 = datetime.now()
-
-    # print(d2-d1)
+    classification_bathy_testing(mini_features, mini_labels)
+    # classification_dummy_testing():

test_area.py
@@ -14,6 +14,16 @@ def sqeucl_dist(x, xs):
         2), axis=2)
     return m
 
+def dK_dls(x, xs):
+    M = np.repeat(x[:,None,:], len(xs), axis=1)
+    M_ds = np.array([[M[:,:,i], M[:,:,i].T] for i in range(x.shape[1])])
+
+
+    # m = np.sum(np.power(
+    #     np.repeat(x[:,None,:], len(xs), axis=1) -
+    #     np.resize(xs, (len(x), xs.shape[0], xs.shape[1])),
+    #     2), axis=2)
+    return m
 
 def build_symbolic_derivatives(X):
     # Pre-calculate derivatives of inverted matrix to substitute values in the Squared Exponential NLL gradient
@@ -65,9 +75,11 @@ def build_symbolic_derivatives(X):
 # print(datetime.now() - t1)
 
 # Sample data
-size = 10
+size = 3
+dims = 2
 print("Data size: {}".format(size))
-data = np.array(np.array(10*np.random.rand(size, size), dtype='int64'), dtype='float64')
+#data = np.array(np.array(10*np.random.rand(size, size), dtype='int64'), dtype='float64')
+data = np.arange(size*dims).reshape(size, dims)
 l_scales = np.random.rand(size)
 f_err = np.random.rand()
 n_err = np.random.rand()
@@ -75,44 +87,61 @@ n_err = np.random.rand()
 ######################### Sympy #########################
 # Symbols
 f_err_sym = sympy.Symbol('f_err')
-data_sym = sympy.MatrixSymbol('m', data.shape[0], data.shape[1])
+# data_sym = sympy.MatrixSymbol('m', data.shape[0], data.shape[1])
 l_scales_sym = sympy.MatrixSymbol('l', 1, data.shape[1])
 n_err_sym = sympy.Symbol("n_err")
 l_scales = l_scales.reshape(1, len(l_scales))
+scaled_data = data/l_scales_sym
+sqeucl_dists = sqeucl_dist(scaled_data, scaled_data)
 
-m = sympy.Matrix(f_err_sym**2 * data/l_scales_sym) # + n_err_sym**2 * np.identity(data.shape[0]))
+# m = sympy.Matrix(f_err_sym**2 * data/l_scales_sym) # + n_err_sym**2 * np.identity(data.shape[0]))
+from ML.gp import GaussianProcess
+gp = GaussianProcess()
+gp.X = data
+m = gp.build_symbolic_K(f_err_sym, l_scales_sym, n_err_sym)
+dK_dls = gp.build_dK_dls(m, l_scales_sym)
 
-# t_t1 = datetime.now()
-# dm_df = m.diff(f_err_sym)
-# dm_df_eval = sympy.lambdify([f_err_sym, l_scales_sym], dm_df)
-# dm_df_eval = autowrap(dm_df, backend='cython')
-# dm_df_eval = binary_function('dm_df', dm_df)
-# t_t2 = datetime.now()
-# print("Sympy creating deriv function: {}".format(t_t2 - t_t1))
-
-# t_t3 = datetime.now()
-# dm_df_eval(f_err, l_scales)
-# t_t4 = datetime.now()
-# print("Sympy evaluating function: {}".format(t_t4 - t_t3))
-
-m_1 = f_err_sym**2 * np.array(data_sym) / np.array(l_scales_sym) + n_err_sym**2 * np.identity(data.shape[0])
+exp_sqeucl_dists = math.e ** (-0.5 * sqeucl_dists)
+m_1 = f_err_sym**2 * exp_sqeucl_dists + n_err_sym**2 * np.identity(data.shape[0])
 m_1 = sympy.Matrix(m_1)
 
-#### Using diff for each respective dimension 
-d_t1 = datetime.now()
-dK_df = m_1.diff(f_err_sym)
-dK_dls = [m_1.diff(l_scale_sym) for l_scale_sym in l_scales_sym]
-dK_dn = m_1.diff(n_err_sym)
-d_t2 = datetime.now()
-print("Using diff for each dimension separately took: {}".format(d_t2 - d_t1))
+# Using diff
+t1 = datetime.now()
+# foo = m_1.diff(f_err_sym)
+# foo = m_1.diff(n_err_sym)
+foo = m_1.diff(l_scales_sym[0])
+print(datetime.now()-t1)
 
-#### Using jacobian all at once instead of individual diffs
-j_t1 = datetime.now()
-d_wrt = sympy.Matrix([f_err_sym] + list(l_scales_sym) + [n_err_sym])
-m_1_flat = sympy.Matrix(sympy.flatten(m_1))
-m_1_ds_flat = m_1_flat.jacobian(d_wrt)
-j_t2 = datetime.now()
-print("Using jacobian over all dimensions took: {}".format(j_t2 - j_t1))
+# Manual
+t2 = datetime.now()
+bar = None
+# bar = sympy.Matrix(np.full(sqeucl_dists.shape, 2*f_err_sym, dtype='object') * exp_sqeucl_dists)
+# bar = 2*f_err_sym * np.exp(-0.5*sqeucl_dists)
+# bar = np.diag(np.array([2*n_err_sym]*data.shape[0]))
+print(datetime.now()-t2)
+
+print(np.sum(np.array(foo) == np.array(bar)))
+
+# #### Using diff for each respective dimension 
+# d_t1 = datetime.now()
+# dK_df = m_1.diff(f_err_sym)
+# dK_dls = [m_1.diff(l_scale_sym) for l_scale_sym in l_scales_sym]
+# dK_dn = m_1.diff(n_err_sym)
+# d_t2 = datetime.now()
+# print("Using diff for each dimension separately took: {}".format(d_t2 - d_t1))
+# 
+# #### Using jacobian all at once instead of individual diffs
+# j_t1 = datetime.now()
+# d_wrt = sympy.Matrix([f_err_sym] + list(l_scales_sym) + [n_err_sym])
+# m_1_flat = sympy.Matrix(sympy.flatten(m_1))
+# m_1_ds_flat = m_1_flat.jacobian(d_wrt)
+# j_t2 = datetime.now()
+# print("Using jacobian over all dimensions took: {}".format(j_t2 - j_t1))
+
+#### Theano
+import theano.tensor as T
+x = theano.shared(data)
+l = theano.shared(l_scales)
 
 # t_t1 = datetime.now()
 # dm1_df = m_1.diff(f_err_sym)

tests.py
@@ -1,22 +1,67 @@
 from ML.gp import GaussianProcess
+from datetime import datetime
 import numpy as np
 import unittest
 from numpy import testing as np_test
 from scipy.spatial.distance import cdist
+from sympy.utilities.autowrap import autowrap
+import sympy as sp
+import math
 
 class TestGPMethods(unittest.TestCase):
 
     def test_multi_length_scale(self):
-        print("Testing if pre-processing of arrays to account for multiple length scales before using cdist is equivalent to the 'dumb' method")
+        print("Testing if pre-processing of arrays to account for multiple length scales before using cdist is equivalent to the 'dumb' method\n")
         gp = GaussianProcess()
         vec = np.random.rand(7,7)
         l_scales = np.random.rand(7)
-        SE_term_manual = (np.sum(np.array([((i-j)/l_scales)**2 for i in vec for j in vec ]), axis=1)
-            .reshape(len(vec), len(vec)))
 
-        SE_term = gp.se_term(vec, vec, l_scales)
+        # Using library
+        SE_term_lib = cdist(vec/l_scales, vec/l_scales, 'sqeuclidean')
 
-        np_test.assert_almost_equal(SE_term, SE_term_manual)
+        # Using custom function (to account for arbitrary objects)
+        SE_term = gp.sqeucl_dist(vec/l_scales, vec/l_scales)
+        # SE_term = gp.se_term(vec, vec, l_scales)
+
+        np_test.assert_almost_equal(SE_term_lib, SE_term)
+
+    def test_dK_dl_equivalence(self):
+        print("Testing if 'fast' creation of dK_dls is equivalent to using sympy matrix differentiation\n")
+
+        # Setup
+        gp = GaussianProcess()
+        dims = 5
+        size = 10
+        vec = np.array(10*np.random.rand(size, dims), dtype='int64')
+        gp.X = vec
+
+        # Symbols
+        f_err_sym = sp.Symbol('f_err')
+        l_scales_sym = sp.MatrixSymbol('l', 1, dims)
+        n_err_sym = sp.Symbol('n_err')
+
+        # Actuals
+        f_err, n_err = np.array([1, 1])
+        l_scales = np.array([1] * dims)
+
+        # Build full K matrix
+        scaled_data = vec/l_scales_sym
+        m = (f_err_sym**2 * math.e ** (-0.5 * gp.sqeucl_dist(vec/l_scales_sym, vec/l_scales_sym)) + 
+            n_err_sym**2 * np.identity(vec.shape[0]))
+        m = sp.Matrix(m)
+
+        # Using sympy differentiations
+        print("Sympy differentiations and evaluation...")
+        # dK_dls_using_lib = [m.diff(l_scale_sym) for l_scale_sym in l_scales_sym]
+        dK_dl_0_sp = m.diff(l_scales_sym[0])
+        eval_lib = np.array(sp.lambdify([f_err_sym, l_scales_sym], dK_dl_0_sp)(f_err, sp.Matrix([l_scales])), dtype='float64')
+
+        print("Closed-form solution differential/evaluation...")
+        m_ = np.exp(-0.5 * gp.dist(vec, vec, l_scales))
+        eval_cf = gp.dK_dls_eval(m_, f_err, l_scales)[0]
+
+        # Check float equalities
+        self.assertEqual(True, np.allclose(eval_lib, eval_cf))
 
 if __name__ == '__main__':
     unittest.main()