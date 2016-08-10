d1b5dc584f2958e13f7dc6627acf4e372121a418
Justin Ting <justingling@gmail.com>
2016-08-11 09:07:33 +1000

    garbage phone dropped to 3g hotspot again

--- ML/gp/bcm.py
+++ ML/gp/bcm.py
@@ -12,18 +12,15 @@ class BCM(GP_ensembles):
     def predict(self, x, keep_probs=False):
         means_gp, vars_gp = self.gp_means_vars(x)
 
-        # NOTE actually correct but incredibly poor results below?
-        # M = vars_gp.shape[0]
-        # vars_bcm_sq_m2 = np.sum(vars_gp**(-2) + (1-M) * vars_gp**(-2), axis=0) # vars
-        # vars_bcm_sq = vars_bcm_sq_m2**(-1)
-        # means_bcm = vars_bcm_sq * np.sum(vars_gp**(-1) * means_gp, axis=0)  # means
-
         # These contain a row for each binary class case (OvR)
         #   AFTER summing along axis 0 (each of the local experts)
         M = vars_gp.shape[0]
         gaus_prec = vars_gp**(-2)
+
+        # TODO prior precision - squared element-wise inverse of diagonal along covariance matrix
         # prior_precision = vars_gp**(-2) # NOTE WRONG - prior precision is diag of elementwise inverse of cov matrix
         prior_precision = np.ones_like(vars_gp)
+
         vars_bcm_inv = np.sum(gaus_prec + (1-M) * prior_precision, axis=0) # variances
         vars_bcm = vars_bcm_inv**(-1)
         means_bcm = vars_bcm * np.sum(gaus_prec * means_gp, axis=0)  # means
