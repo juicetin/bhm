fixing prior variance

ML/gp/bcm.py
@@ -19,9 +19,10 @@ class BCM(GP_ensembles):
 
         # TODO prior precision - squared element-wise inverse of diagonal along covariance matrix
         # prior_precision = vars_gp**(-2) # NOTE WRONG - prior precision is diag of elementwise inverse of cov matrix
-        prior_precision = np.ones_like(vars_gp)
+        # prior_precision = np.ones_like(vars_gp)
+        prior_precisions = np.array([gp_expert.prior_variance(x) for gp_expert in self.gp_experts]).reshape(gaus_prec)
 
-        vars_bcm_inv = np.sum(gaus_prec + (1-M) * prior_precision, axis=0) # variances
+        vars_bcm_inv = np.sum(gaus_prec + (1-M) * prior_precisions, axis=0) # variances
         vars_bcm = vars_bcm_inv**(-1)
         means_bcm = vars_bcm * np.sum(gaus_prec * means_gp, axis=0)  # means
 

loaddata.py
@@ -272,10 +272,10 @@ if __name__ == "__main__":
 
     ########################################### Product of Experts ###########################################
 
-    # size = 13000
-    # idx = mini_batch_idxs(labels_simple, size, 'stratified')
+    size = 100
+    train_idx = mini_batch_idxs(labels_simple, size, 'even')
 
-    train_idx = np.load('data/semi-optimal-1000-subsample.npy')
+    # train_idx = np.load('data/semi-optimal-1000-subsample.npy')
 
     # gp = GaussianProcess()
     # gp_stats = testGP(gp, features_sn, labels_simple, train_idx, n_iter=2)