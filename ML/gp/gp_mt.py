b66950ef864276ab47c24145a0fdae2a308a9638
Justin Ting <justingling@gmail.com>
2016-08-20 15:41:41 +1000

    moving machines

--- ML/gp/gp_mt.py
+++ ML/gp/gp_mt.py
@@ -7,24 +7,28 @@ class GPMT(GaussianProcess):
     
     def fit_multi_task(self, X, y):
         self.N = self.X.shape[0]
-        self.M = np.unique(y).shape[0]
+        self.M = self.X.shape[1]
+
+        self.X = X
+        self.y = y
+        # NxM matrix Y such that y = vecY
+        # y_il is response for l-th task on i-th input x_i
+        self.Y = y.reshape(X.shape)
 
         bounds = [[0,None]] * (X.shape[1] + 2)
-        x0 = np.random.rand(2 + X.shape[1])
+        x0 = np.random.rand(2 + X.shape[1]) # Initial guess
+
+        # Update current params to the initial guesses
+        self.f_err, self.l_scales, self.n_err = self.unpack_GP_args(x0)
+        # TODO initial Kf (???)
+
+        # Optimise over parameters for multi-task GP
         res = minimize(self.L_comp_NLL, x0, method='l-bfgs-b', bounds=bounds)
 
         args = self.unpack_GP_args(res['x'])
 
     def predict_multi_task(self, x):
-        Kf
-        Kx
-        task_variances
-        D = np.diag(task_variances)
-        I = np.identity(Kf.shape[0]) # Not sure about shape of identity matrix here
-        sigma = np.kron(Kf, Kx) + np.kron(D, I)
-
-        means = np.kron(Kf, Kx).T.dot(np.inv(sigma)).dot(y)
-        return means
+        return self.F(x)
 
     # Returns lower triangular Cholesky decomposition for complete-data log-likelihood
     def L_comp_NLL(self, args):
@@ -33,9 +37,9 @@ class GPMT(GaussianProcess):
         N = self.N
         M = self.M
         Kf = self.Kf()
-        Kx = self.Kx()
+        Kx = self.Kx(f_err, l_scales, n_err)
         F = self.F()
-        Y = self.Y()
+        Y = self.Y
         task_variances
         D = np.diag(task_variances)
         task_variances
@@ -62,17 +66,34 @@ class GPMT(GaussianProcess):
     def Kf(self, f_err, l_scales, n_err):
         Kx = self.Kx(f_err, l_scales, n_err)
         N = self.N
-        F
+        F = self.F(self.X)
         Kf = 1/N * F.T.dot(np.inv(Kx)).dot(F)
         return Kf
 
     # Returns updated noise over tasks
     def task_noise(self):
         N = self.N
-        Y
-        F
+        Y = self.Y
+        F = self.F(self.X)
+
         task_variances = 1/N * (Y-F).T.dot(Y-F)
         return task_variances
+    
+    # Vector of function values corresponding to Y
+    #   Inference is needed when learning hyperparameters for noisy observations
+    def F(self, x):
+        f_err, l_scales, n_err = self.cur_thetas()
+
+        Kf # MxM
+        Kx # NxN
+        task_variances # 1xM
+        D = np.diag(task_variances) # MxM
+        I = np.identity(x.shape[0]) # NxN
+        sigma = np.kron(Kf, Kx) + np.kron(D, I) # MNxMN
+        y = self.y # ???
+
+        means = np.kron(Kf, Kx).T.dot(np.inv(sigma)).dot(y)
+        return means
 
     # Covariance functions over inputs
     # NOTE stationary covariance functions as K^f explains variance
@@ -80,13 +101,6 @@ class GPMT(GaussianProcess):
     def Kx(self, f_err, l_scales, n_err):
         # TODO follow NOTE from comment above function
         return self.K_se(f_err, l_scales, n_err)
-    
-    # NxM matrix Y such that y = vecY
-    # y_il is response for l-th task on i-th input x_i
-    def Y(self):
-        pass
 
-    # Vector of function values corresponding to Y
-    def F(self):
-        pass
-
+    def cur_thetas(self):
+        return self.f_err, l_scales, n_err
