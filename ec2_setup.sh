c75d200d0423ef541e1dd2ae1092b001b912eee7
Justin Ting <justinlging@gmail.com>
2016-10-27 16:03:49 +1100

    ec2 setup script

--- ec2_setup.sh
+++ ec2_setup.sh
@@ -11,13 +11,13 @@ eval $(ssh-agent -s)
 ssh-add
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 localectl set-locale LANG=en_AU.UTF-8
+
 wget https://github.com/jyting/arch-system/raw/master/.zshrc
 wget https://github.com/jyting/arch-system/raw/master/.vimrc
 wget https://github.com/jyting/arch-system/raw/master/.tmux.conf
 wget https://github.com/jyting/arch-system/raw/master/.aliasrc
 wget https://github.com/jyting/arch-system/raw/master/.exportrc
 wget https://github.com/jyting/arch-system/raw/master/.condarc
-
 
 wget https://transfer.sh/nT4UX/conda-list &
 wget https://transfer.sh/4Fe0x/red-data.zip &
@@ -32,5 +32,5 @@ wget https://transfer.sh/saA3U/anaconda3-4.2.0-linux-x86-64.sh &
 bash Anaconda3-4.2.0-Linux-x86_64.sh
 conda install --file $HOME/conda-list -n root
 
-wget https://transfer.sh/ml3vM/gp-preds.zip &
-wget https://transfer.sh/maWKP/dm-mcmc-chains.zip &
+# wget https://transfer.sh/ml3vM/gp-preds.zip &
+# wget https://transfer.sh/maWKP/dm-mcmc-chains.zip &
